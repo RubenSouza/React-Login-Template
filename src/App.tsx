f08b7babfc33481934d4624f32633c60aaeb8492
RubenSouza <rubem.eliel20212@gmail.com>
2023-05-05 21:47:51 -0300

    Loading Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import jwtDecode from "jwt-decode";
 import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
 
 function App() {
   const user = JSON.parse(localStorage.getItem("user") as string) || null;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isLogged, setIsLogged] = useState<boolean>(false);
 
   useEffect(() => {
+    setIsLoading(false);
     if (user) {
       const decodedToken: any = jwtDecode(user?.accessToken);
 
@@ -25,22 +28,25 @@ function App() {
     }
 
     console.log(user);
-  }, [user]);
+  }, [user, isLoading]);
 
-  return (
-    <div className="bg-[#121212] h-screen max-h-screen text-white ">
-      {isLogged ? (
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      )}
-    </div>
-  );
+  if (isLoading) return <Loading />;
+  else {
+    return (
+      <div className="bg-[#121212] h-screen max-h-screen text-white ">
+        {isLogged ? (
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
