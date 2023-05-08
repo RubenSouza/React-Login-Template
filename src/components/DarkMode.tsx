fde36fd2cd38784ac6da153794677a490056244e
RubenSouza <rubem.eliel20212@gmail.com>
2023-05-08 10:21:15 -0300

    dark mode transition animation

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -31,18 +31,20 @@ const DarkMode = () => {
   return (
     <div
       className={`absolute top-4 right-5 w-24 h-11 border-[2px]
-     rounded-full border-[#c5c5c5] bg-[#f2f2f2] hover:cursor-pointer ${
+     rounded-full border-[#c5c5c5] bg-[#f2f2f2] transition-colors duration-300 ease-linear hover:cursor-pointer ${
        isDark ? "bg-primary-700 border-primary-400" : ""
      }`}
     >
       <div className="flex flex-row justify-between">
         <BsSun
           className={`w-10  h-10 text-primary-100 rounded-full p-[7px]
+          transition-colors duration-200 ease-in  
            ${!isDark ? "bg-yellow-500" : ""}`}
           onClick={handleIsDark}
         />
         <MdOutlineDarkMode
           className={`w-10  h-10 text-primary-600 rounded-full p-[6px]
+          transition-colors duration-200 ease-in  
             ${isDark ? "bg-primary-300 text-primary-50 shadow-md" : ""}`}
           onClick={handleIsDark}
         />
